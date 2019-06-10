2b543f912e2366215895aa6f269a68d8b3c7e23d
Kia Ishii <kia.king.08@gmail.com>
2019-06-10 16:15:42 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'vue/attributes-order': 'off',
+    'vue/component-name-in-template-casing': ['warn', 'PascalCase', {
+      ignores: [
+        'component', 'no-ssr', 'nuxt', 'nuxt-child', 'nuxt-link', 'transition',
+        'transition-group',
+      ]
+    }],
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-v-html': 'off',
+    'vue/singleline-html-element-content-newline': 'off'
+  }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Sefirot
+
+The Global Brain Design System Components.
+
+## Contribution
+
+We're really excited that you are interested in contributing to Sefirot. Before submitting your contribution though, please make sure to take a moment and read through the following guidelines.
+
+### Code style guide
+
+> Every line of code should appear to be written by a single person, no matter the number of contributors.
+> &mdash; <cite>@mdo</cite>
