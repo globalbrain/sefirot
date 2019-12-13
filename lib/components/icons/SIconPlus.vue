e46b6cd023ba161543dab79393e2fd3e7eb5fd74
Kia Ishii <kia.king.08@gmail.com>
2019-12-13 10:02:36 +0900

    feat: add plus icon

---
+++ lib/components/icons/SIconPlus.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M22,11H13V2a1,1,0,0,0-2,0v9H2a1,1,0,0,0,0,2h9v9a1,1,0,0,0,2,0V13h9a1,1,0,0,0,0-2Z" />
+  </svg>
+</template>
