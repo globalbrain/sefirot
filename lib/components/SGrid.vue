20ff160841d68e1410492b571caf5f2bbce2104a
Kia King Ishii <kia.king.08@gmail.com>
2020-09-04 16:19:05 +0900

    feat: add header and footer support for SGrid component

--- lib/components/SGrid.vue
+++ lib/components/SGrid.vue
@@ -1,48 +1,57 @@
 <template>
   <div class="SGrid">
-    <div class="wrapper">
-      <div class="container">
-        <div v-if="$slots.columns" class="columns">
-          <div class="columns-row">
-            <slot name="columns" />
-          </div>
-        </div>
+    <div v-if="$slots.header" class="header">
+      <slot name="header" />
+    </div>
 
-        <div class="body">
-          <div class="body-records" :class="{ loading }">
-            <div
-              v-for="record in records"
-              :key="record[primaryKey]"
-              class="row"
-              :class="{ clickable }"
-              role="button"
-              @click="handleClick(record)"
-            >
-              <slot :record="record" />
-
-              <div v-if="actions" class="actions">
-                <component
-                  :is="actionComponent"
-                  :action="actions"
-                  :actions="actions"
-                />
-              </div>
-            </div>
-          </div>
-
-          <transition name="fade">
-            <div v-if="loading" class="body-loading">
-              <SIconPreloaderDark class="body-loading-icon" />
-            </div>
-          </transition>
+    <VueSimplebar class="body">
+      <div v-if="$slots.columns" class="columns">
+        <div class="columns-row">
+          <slot name="columns" />
         </div>
       </div>
+
+      <div v-if="records.length > 0" class="body-records" :class="{ loading }">
+        <div
+          v-for="record in records"
+          :key="record[primaryKey]"
+          class="row"
+          :class="{ clickable }"
+          role="button"
+          @click="handleClick(record)"
+        >
+          <slot :record="record" />
+
+          <div v-if="actions" class="actions">
+            <component
+              :is="actionComponent"
+              :action="actions"
+              :actions="actions"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div v-else class="body-empty">
+        <slot name="empty" />
+      </div>
+    </VueSimplebar>
+
+    <div v-if="$slots.footer" class="footer">
+      <slot name="footer" />
     </div>
+
+    <transition name="fade">
+      <div v-if="loading" class="body-loading">
+        <SIconPreloaderDark class="body-loading-icon" />
+      </div>
+    </transition>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed } from '@vue/composition-api'
+import VueSimplebar from 'simplebar-vue'
 import SIconPreloaderDark from './icons/SIconPreloaderDark.vue'
 import SGridActionLink from './SGridActionLink.vue'
 import SGridActionSingle from './SGridActionSingle.vue'
@@ -50,6 +59,7 @@ import SGridActionMulti from './SGridActionMulti.vue'
 
 export default defineComponent({
   components: {
+    VueSimplebar,
     SIconPreloaderDark
   },
 
@@ -90,24 +100,8 @@ export default defineComponent({
 @import "@/assets/styles/variables";
 
 .SGrid {
-  margin: -8px -8px -16px;
-  min-width: 100%;
-  overflow: hidden;
-  overflow-x: auto;
-}
-
-.wrapper {
-  display: inline-flex;
-  flex-direction: column;
-  padding: 8px 8px 16px;
-  min-width: 100%;
-}
-
-.container {
-  display: inline-flex;
-  flex-direction: column;
-  border-radius: 4px;
-  background-color: var(--c-white);
+  border-radius: 8px;
+  background-color: var(--c-bg);
   box-shadow: var(--shadow-depth-1);
 }
 
@@ -117,12 +111,12 @@ export default defineComponent({
 
 .columns-row {
   display: inline-flex;
-  padding: 0 16px;
+  padding: 0 12px;
 
-  .cell {
+  & >>> .cell {
     flex-grow: 1;
     flex-shrink: 0;
-    padding: 16px 16px 12px;
+    padding: 16px 12px 12px;
     font-size: 12px;
     font-weight: 700;
     color: var(--c-text-light-2);
@@ -131,20 +125,20 @@ export default defineComponent({
 
 .body {
   position: relative;
+  overflow: hidden;
+  overflow-x: auto;
+}
+
+.body-records {
+  display: inline-flex;
+  flex-direction: column;
   border-top: 1px solid var(--c-divider-lighter);
+  min-width: 100%;
 
-  .body-records {
-    transition: opacity .25s;
-  }
-
-  .body-records.loading {
-    opacity: 0;
-  }
-
-  .row {
+  & >>> .row {
     display: flex;
     border-top: 1px solid var(--c-divider-lighter);
-    padding: 22px 16px 21px;
+    padding: 22px 12px 21px;
     transition: background-color .25s;
     cursor: default;
 
@@ -161,10 +155,10 @@ export default defineComponent({
     }
   }
 
-  .cell {
+  & >>> .cell {
     flex-shrink: 0;
     margin: 0;
-    padding: 0 16px;
+    padding: 0 12px;
     line-height: 20px;
     font-size: 14px;
     color: var(--c-text-light-1);
@@ -182,7 +176,7 @@ export default defineComponent({
     }
   }
 
-  .cell-main {
+  & >>> .cell-main {
     margin: 0;
     width: 100%;
     white-space: nowrap;
@@ -190,7 +184,7 @@ export default defineComponent({
     text-overflow: ellipsis;
   }
 
-  .cell-sub {
+  & >>> .cell-sub {
     margin: 0;
     width: 100%;
     font-size: 12px;
@@ -201,28 +195,37 @@ export default defineComponent({
     text-overflow: ellipsis;
   }
 
-  .actions {
+  & >>> .actions {
     flex-grow: 1;
     height: 100%;
   }
 }
 
+.footer {
+  border-top: 1px solid var(--c-divider);
+}
+
 .body-loading {
   position: absolute;
-  top: 24px;
-  left: 50%;
-  transform: translateX(-50%);
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: var(--c-bg);
   transition: opacity .25s, transform .25s;
 }
 
 .body-loading.fade-enter,
 .body-loading.fade-leave-to {
   opacity: 0;
-  transform: translateX(-50%) scale(.8);
 }
 
 .body-loading-icon {
+  position: absolute;
+  top: 24px;
+  left: 50%;
   width: 48px;
   height: 48px;
+  transform: translateX(-50%);
 }
 </style>
