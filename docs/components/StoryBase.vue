e42809afa220cd17507cfe198bf3dc2adf90bd77
Kia King Ishii <kia.king.08@gmail.com>
2021-01-21 12:40:54 +0900

    chore: make examples look much better

--- docs/components/StoryBase.vue
+++ docs/components/StoryBase.vue
@@ -1,6 +1,14 @@
 <template>
-  <article class="StoryBase">
-    <h1 v-if="title" class="title">{{ title }}</h1>
+  <article ref="el" class="StoryBase" :class="{ open: isOpen }">
+    <div class="header">
+      <h1 class="title">{{ title }}</h1>
+
+      <div class="action">
+        <button class="button" @click="toggle">
+          <SIconCode class="icon" />
+        </button>
+      </div>
+    </div>
 
     <div class="component">
       <slot />
@@ -9,11 +17,35 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+import SIconCode from '@@/lib/components/icons/SIconCode.vue'
 
 export default defineComponent({
+  components: {
+    SIconCode
+  },
+
   props: {
     title: { type: String, default: null }
+  },
+
+  setup() {
+    const el = ref<HTMLElement | null>(null)
+    const isOpen = ref(false)
+
+    function toggle(): void {
+      isOpen.value = !isOpen.value
+
+      const style = isOpen.value ? 'block' : 'none'
+
+      el.value?.nextElementSibling?.setAttribute('style', `display: ${style};`)
+    }
+
+    return {
+      el,
+      isOpen,
+      toggle
+    }
   }
 })
 </script>
@@ -26,8 +58,7 @@ export default defineComponent({
   margin-right: -24px;
   margin-left: -24px;
   border-top: 1px solid var(--c-divider-light);
-  border-radius: 0;
-  border-bottom: 0;
+  border-bottom: 1px solid var(--c-divider-light);
 
   @media (min-width: 375px) {
     margin-right: -32px;
@@ -39,26 +70,69 @@ export default defineComponent({
     margin-left: -24px;
     border-right: 1px solid var(--c-divider-light);
     border-left: 1px solid var(--c-divider-light);
+    border-radius: 8px;
+  }
+}
+
+.StoryBase.open {
+  border-bottom: 0;
+
+  @media (min-width: 768px) {
     border-radius: 4px 4px 0 0;
   }
 }
 
+.header {
+  display: flex;
+  border-bottom: 1px solid var(--c-divider-light);
+  padding: 0 8px 0 24px;
+  background-color: var(--c-white-soft);
+
+  @media (min-width: 375px) {
+    padding: 4px 12px 4px 32px;
+  }
+
+  @media (min-width: 768px) {
+    border-radius: 8px 8px 0 0;
+    padding: 4px 4px 4px 24px;
+  }
+}
+
 .title {
+  flex-grow: 1;
   margin: 0;
-  padding: 12px 24px 0;
+  padding-top: 8px;
   line-height: 24px;
   font-family: var(--font-family-base);
   font-size: 14px;
   font-weight: 500;
   color: var(--c-text-light-2);
+}
 
-  @media (min-width: 375px) {
-    padding: 16px 32px 0;
-  }
+.action {
+  flex-shrink: 0;
+}
 
-  @media (min-width: 768px) {
-    padding: 24px 24px 0;
+.button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 6px;
+  width: 40px;
+  height: 40px;
+  color: var(--c-text-2);
+  transition: color .25s, background-color .25s;
+
+  &:hover {
+    color: var(--c-info);
+    background-color: var(--c-white-mute);
   }
+}
+
+.icon {
+  width: 20px;
+  height: 20px;
+  fill: currentColor;
 }
 
 .component {
