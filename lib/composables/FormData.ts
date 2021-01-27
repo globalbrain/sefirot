0622618af60646041a1cafb027ac68370c3c3b61
Kia King Ishii <kia.king.08@gmail.com>
2021-01-27 13:18:42 +0900

    feat(form): add callback data creation support in useData composable

--- lib/composables/FormData.ts
+++ lib/composables/FormData.ts
@@ -2,14 +2,20 @@ import { reactive } from '@vue/composition-api'
 
 export interface Data<T extends State> {
   data: T
+  init: InitFunction
   update: UpdateFunction
 }
 
 export type State = Record<string, any>
+export type InitFunction = () => void
 export type UpdateFunction = (model: string, value: any) => void
 
-export function useFormData<T extends State>(data: T): Data<T> {
-  const state = reactive(data)
+export function useFormData<T extends State>(data: T | (() => T)): Data<T> {
+  const state = reactive(getData(data))
+
+  function init(): void {
+    initData(state, getData(data) as any)
+  }
 
   function update(model: string, value: any): void {
     updateData(state, model, value)
@@ -17,8 +23,17 @@ export function useFormData<T extends State>(data: T): Data<T> {
 
   return {
     data: state as T,
+    init,
     update
   }
+}
+
+function getData<T extends State>(data: T | (() => T)): T {
+  return data instanceof Function ? data() : data
+}
+
+function initData<T extends State>(data: T, source: T): void {
+  Object.assign(data, source)
 }
 
 function updateData<T extends State>(data: T, model: string, value: any): void {
