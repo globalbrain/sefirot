6a8497ba4f0941df1c9e4e7716034b0a447ba8ea
ryo-gk <ryo.gk55@gmail.com>
2021-02-03 12:19:14 +0900

    fix: rules for max length validator

--- lib/validation/rules/maxLength.ts
+++ lib/validation/rules/maxLength.ts
@@ -1,11 +1,11 @@
 import { maxLength as baseMaxLength } from '../validators'
 import { Rule } from './'
 
-export default function maxLength(max: string, message?: string): Rule {
+export default function maxLength(max: number, message?: string): Rule {
   return {
     name: 'maxLength',
     message: message ?? `The value must be less than ${max} characters.`,
     optional: true,
-    validate: (value, data) => baseMaxLength(value, data[max])
+    validate: value => baseMaxLength(value, max)
   }
 }
