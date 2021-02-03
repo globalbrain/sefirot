8c420a73181452ace33643bdfd8aab03fde43690
ryo-gk <ryo.gk55@gmail.com>
2021-02-03 11:28:51 +0900

    Revert "feat(validation): fix rule for max value validation"
    This reverts commit 53a6c300f19e6f65877dd12c890bdf4efd768a0a.

--- lib/validation/rules/maxValue.ts
+++ lib/validation/rules/maxValue.ts
@@ -1,11 +1,11 @@
 import { maxValue as baseMaxValue } from '../validators'
 import { Rule } from './'
 
-export default function maxValue(max: number, message?: string): Rule {
+export default function maxValue(max: string, message?: string): Rule {
   return {
     name: 'maxValue',
     message: message ?? `The value must be ${max} or less.`,
     optional: true,
-    validate: value => baseMaxValue(value, max)
+    validate: (value, data) => baseMaxValue(value, data[max])
   }
 }
