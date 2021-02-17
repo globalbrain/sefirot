88019c5f70c9427fd2d67b2c2671fc212e49129a
Kia King Ishii <kia.king.08@gmail.com>
2021-02-17 16:37:50 +0900

    feat(support): expose `orderBy` and `groupBy` functions

--- lib/support/Util.ts
+++ lib/support/Util.ts
@@ -1,5 +1,7 @@
 export { default as isEqual } from 'lodash-es/isEqual'
 export { default as cloneDeep } from 'lodash-es/cloneDeep'
+export { default as orderBy } from 'lodash-es/orderBy'
+export { default as groupBy } from 'lodash-es/groupBy'
 
 export function isNullish(value: any): value is undefined | null {
   return value === null || value === undefined
