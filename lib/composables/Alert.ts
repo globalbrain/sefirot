d738a5bc9874bc0ceed9df6370038ee61049e768
Cue <1493221+cuebit@users.noreply.github.com>
2021-03-16 02:38:22 +0000

    feat(alert): improve composition

--- lib/composables/Alert.ts
+++ lib/composables/Alert.ts
@@ -1,5 +1,6 @@
 import { Values } from '../types/Utils'
 import { ButtonAction } from './Action'
+import { useStore } from './Store'
 
 export interface Alert {
   type?: AlertMode
@@ -17,6 +18,19 @@ export const AlertModes = {
   Danger: 'danger'
 } as const
 
-export function useAlert(alert: Alert): Alert {
-  return alert
+export function useAlert(alert: Alert) {
+  const store = useStore()
+
+  function open(): void {
+    store.dispatch('alert/open', alert)
+  }
+
+  function close(): void {
+    store.dispatch('alert/close')
+  }
+
+  return {
+    open,
+    close
+  }
 }
