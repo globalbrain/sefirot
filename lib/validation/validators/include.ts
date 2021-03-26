a9add420372d9dd1637711fc21e67a73c267cf82
ryo-gk <ryo.gk55@gmail.com>
2021-03-26 15:29:23 +0900

    feat(validation): include() can recieve array as 'other' arg

--- lib/validation/validators/include.ts
+++ lib/validation/validators/include.ts
@@ -1,5 +1,7 @@
 import { isEqual } from '../../support/Util'
 
 export default function include(value: any[], other: any): boolean {
-  return value.some(v => isEqual(v, other))
+  return Array.isArray(other)
+    ? value.some(v => other.some(o => isEqual(v, o)))
+    : value.some(v => isEqual(v, other))
 }
--- test/validation/validators/include.spec.ts
+++ test/validation/validators/include.spec.ts
@@ -1,6 +1,6 @@
 import { include } from 'sefirot/validation/validators'
 
-type Check = [any[], any, boolean]
+type Check = [any[], any | any[], boolean]
 
 describe('validation/validators/include', () => {
   test('it validates if the value includes the given another value', () => {
@@ -9,7 +9,9 @@ describe('validation/validators/include', () => {
       [['0'], 0, false],
       [[1, 2, 3], 2, true],
       [[{ id: 1 }], { id: 1 }, true],
-      [[{ id: 1 }], { id: 2 }, false]
+      [[{ id: 1 }], { id: 2 }, false],
+      [[1, 2, 3], [1, 4], true],
+      [[1, 2, 3], [4, 5], false]
     ]
 
     checks.forEach((check) => {
