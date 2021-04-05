0497a1ad0440986bdf2043e9a4f5f4ae9857e920
Ryo_gk <62658104+ryo-gk@users.noreply.github.com>
2021-04-05 15:52:48 +0900

    feat(form): update form apis (#54)
    Co-authored-by: Kia King Ishii <kia.king.08@gmail.com>

--- lib/composables/Form.ts
+++ lib/composables/Form.ts
@@ -1,5 +1,50 @@
-export * from './FormData'
+import { UnwrapRef, reactive } from '@vue/composition-api'
+import { Validation, Rules, useFormValidation } from './FormValidation'
+
 export * from './FormValidation'
 
-export { useFormData as useData } from './FormData'
-export { useFormValidation as useValidation } from './FormValidation'
+export interface Form<T extends Record<string, any>> {
+  data: Data<T>
+  validation?: Validation
+  init(): void
+  update: UpdateFunction<T>
+}
+
+export type Data<T extends Record<string, any>> = UnwrapRef<T>
+
+export type UpdateFunction<
+  T extends Record<string, any>
+> = (model: keyof Data<T>, value: any) => void
+
+export interface UseFormOptions<T extends Record<string, any>> {
+  data: T | (() => T)
+  rules?: Rules
+}
+
+export function useForm<T extends Record<string, any>>(options: UseFormOptions<T>): Form<T> {
+  const initialData = getData(options.data)
+  const rules = options.rules
+
+  const data = reactive(initialData)
+  const validation = rules ? useFormValidation(data, rules) : undefined
+
+  function init(): void {
+    Object.assign(data, initialData)
+    validation?.$reset()
+  }
+
+  function update(model: keyof Data<T>, value: any): void {
+    data[model] = value
+  }
+
+  return {
+    data,
+    validation,
+    init,
+    update
+  }
+}
+
+function getData<T extends Record<string, any>>(data: T | (() => T)): T {
+  return data instanceof Function ? data() : data
+}
