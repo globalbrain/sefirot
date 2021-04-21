449255abc909b4ad8c856cb67daf2214769a515b
Kia King Ishii <kia.king.08@gmail.com>
2021-04-21 15:19:50 +0900

    wip: working on

---
+++ lib/components/SModal.vue
@@ -0,0 +1,147 @@
+<template>
+  <transition name="fade" :duration="250" appear>
+    <div class="SModal" @click="closeIfClosable">
+      <transition name="fade">
+        <div v-show="show" class="content" :style="contentStyles" @click.stop>
+          <component
+            :if="resolvedComponent"
+            :is="resolvedComponent"
+            v-bind="data"
+            @close="close"
+          />
+        </div>
+      </transition>
+    </div>
+  </transition>
+<!--     <template v-if="mount">
+      <transition name="fade" mode="out-in" appear>
+        <div v-show="show" :key="name" class="SModalBase" :class="{ first }" @click="close">
+          <slot />
+        </div>
+      </transition>
+    </template> -->
+</template>
+
+<script lang="ts">
+import { PropType, defineComponent, ref, computed } from '@vue/composition-api'
+import { SyntheticMouseEvent } from '../types/Utils'
+import { useStore } from '../composables/Store'
+
+export default defineComponent({
+  props: {
+    component: null,
+    data: { type: Object, default: () => ({}) },
+    show: { type: Boolean, required: true },
+    width: { type: String, default: 'auto' },
+    closable: { type: Boolean, default: true }
+    // name: { type: String, required: true },
+    // closable: { type: Boolean, default: true }
+  },
+
+  setup(props, { emit }) {
+    const contentStyles = computed(() => ({
+      maxWidth: props.width
+    }))
+
+    const resolvedComponent = ref(null)
+
+    props.component instanceof Function
+      ? props.component().then(module => { resolvedComponent.value = module.default })
+      : (resolvedComponent.value = props.component)
+
+    function close() {
+      emit('close')
+    }
+
+    function closeIfClosable() {
+      props.closable && close()
+    }
+
+    return {
+      contentStyles,
+      resolvedComponent,
+      close,
+      closeIfClosable
+    }
+
+    // const store = useStore()
+
+    // const first = computed(() => store.state.modal.first)
+    // const mount = computed(() => store.state.modal.history.some(h => h.name === props.name))
+    // const show = computed(() => props.name === store.state.modal.name)
+
+    // function close(e: SyntheticMouseEvent): void {
+    //   if (props.closable) {
+    //     if (!isDescendant(e.target)) {
+    //       store.dispatch('modal/close')
+    //     }
+    //   }
+    // }
+
+    // function isDescendant(el: Element): boolean {
+    //   if (el.classList && el.classList.contains('SModalBase')) {
+    //     return false
+    //   }
+
+    //   const parent = document.getElementsByClassName('modal-content')[0]
+
+    //   if (parent && parent.contains(el)) {
+    //     return true
+    //   }
+
+    //   return false
+    // }
+
+    // return {
+    //   first,
+    //   mount,
+    //   show,
+    //   close
+    // }
+  }
+})
+</script>
+
+<style lang="postcss" scoped>
+@import "@/assets/styles/variables";
+
+.SModal {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 100vh;
+  padding: 32px 16px 96px;
+  overflow: hidden;
+  overflow-y: auto;
+
+  @media (min-width: 768px) {
+    padding: 48px 24px 96px;
+  }
+}
+
+.SModal.fade-enter-active .content,
+.SModal.fade-leave-active .content,
+.content.fade-enter-active,
+.content.fade-leave-active {
+  transition: opacity .25s, transform .25s;
+}
+
+.SModal.fade-enter .content,
+.content.fade-enter {
+  opacity: 0;
+  transform: translateY(8px);
+}
+
+.SModal.fade-leave-to .content,
+.content.fade-leave-to {
+  opacity: 0;
+  transform: translateY(-8px);
+}
+
+.content {
+  margin: 0 auto;
+  transition: opacity .25s, transform .25s;
+}
+</style>
--- lib/components/SPortalModals.vue
+++ lib/components/SPortalModals.vue
@@ -1,89 +1,123 @@
 <template>
-  <div class="SPortalModals">
-    <transition name="fade">
-      <div v-if="show" class="backdrop" />
-    </transition>
+  <transition name="fade">
+    <div v-if="isActive" class="SPortalModals">
+      <SModal
+        v-for="(item, index) in items"
+        :key="index"
+        :show="index === items.length - 1"
+        :component="item.component"
+        :data="item.data"
+        :width="item.options && item.options.width"
+        :closable="item.options && item.options.closable"
+        @close="close"
+      />
 
-    <template v-for="dialog in dialogs">
-      <SDialog :key="dialog.name" :name="dialog.name" v-bind="dialog.data" />
-    </template>
+  <!--     <template v-for="dialog in dialogs">
+        <SDialog :key="dialog.name" :name="dialog.name" v-bind="dialog.data" />
+      </template>
 
-    <template v-for="alert in alerts">
-      <SAlert :key="alert.name" :name="alert.name" v-bind="alert.data" />
-    </template>
+      <template v-for="alert in alerts">
+        <SAlert :key="alert.name" :name="alert.name" v-bind="alert.data" />
+      </template>
 
-    <div ref="el" class="modal-content" :class="{ show }">
-      <portal-target name="modal" multiple />
+      <div ref="el" class="modal-content" :class="{ show }">
+        <portal-target name="modal" multiple />
+      </div> -->
     </div>
-  </div>
+  </transition>
 </template>
 
 <script lang="ts">
 import { disableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock'
 import { defineComponent, ref, computed, watch } from '@vue/composition-api'
 import { useStore } from '../composables/Store'
-import { useRoute } from '../composables/Router'
-import SDialog from './SDialog.vue'
-import SAlert from './SAlert.vue'
+// import { useRoute } from '../composables/Router'
+import SModal from './SModal.vue'
+// import SDialog from './SDialog.vue'
+// import SAlert from './SAlert.vue'
 
 export default defineComponent({
   components: {
-    SDialog,
-    SAlert
+    SModal
+    // SDialog,
+    // SAlert
   },
 
   setup() {
     const store = useStore()
-    const route = useRoute()
 
-    const el = ref<Element | null>(null)
+    const items = computed(() => store.state.modal.items)
+    const hasItem = computed(() => items.value.length > 0)
 
-    const show = ref(false)
+    const isActive = ref(false)
 
-    const active = computed(() => store.state.modal.history.length > 0)
-    const current = computed(() => store.state.modal.name)
-
-    const dialogs = computed(() => {
-      return store.state.modal.history.filter(h => h.name.startsWith('dialog'))
+    watch(hasItem, (value) => {
+      value
+        ? (isActive.value = true)
+        : setTimeout(() => { isActive.value = false }, 250)
     })
 
-    const alerts = computed(() => {
-      return store.state.modal.history.filter(h => h.name.startsWith('alert'))
-    })
-
-    watch(active, (value) => { value ? open() : close() })
-
-    watch(current, () => {
-      setTimeout(() => { el.value && el.value.scrollTo(0, 0) }, 250)
-    })
-
-    watch(route, () => { store.dispatch('modal/close') })
-
-    function open(): void {
-      show.value = true
-      lock()
-    }
-
-    function close(): void {
-      show.value = false
-      release()
-    }
-
-    function lock(): void {
-      el.value && disableBodyScroll(el.value, { reserveScrollBarGap: true })
-    }
-
-    function release(): void {
-      el.value && el.value.scrollTo(0, 0)
-      clearAllBodyScrollLocks()
+    function close() {
+      store.dispatch('modal/close')
     }
 
     return {
-      el,
-      show,
-      dialogs,
-      alerts
+      items,
+      hasItem,
+      isActive,
+      close
     }
+
+    // const route = useRoute()
+
+    // const el = ref<Element | null>(null)
+
+    // const show = ref(false)
+
+    // const active = computed(() => store.state.modal.history.length > 0)
+    // const current = computed(() => store.state.modal.name)
+
+    // const dialogs = computed(() => {
+    //   return store.state.modal.history.filter(h => h.name.startsWith('dialog'))
+    // })
+
+    // const alerts = computed(() => {
+    //   return store.state.modal.history.filter(h => h.name.startsWith('alert'))
+    // })
+
+    // watch(active, (value) => { value ? open() : close() })
+
+    // watch(current, () => {
+    //   setTimeout(() => { el.value && el.value.scrollTo(0, 0) }, 250)
+    // })
+
+    // watch(route, () => { store.dispatch('modal/close') })
+
+    // function open(): void {
+    //   show.value = true
+    //   lock()
+    // }
+
+    // function close(): void {
+    //   show.value = false
+    //   release()
+    // }
+
+    // function lock(): void {
+    //   el.value && disableBodyScroll(el.value, { reserveScrollBarGap: true })
+    // }
+
+    // function release(): void {
+    //   el.value && el.value.scrollTo(0, 0)
+    //   clearAllBodyScrollLocks()
+    // }
+
+    // return {
+    //   el,
+    //   show,
+    //   dialogs,
+    //   alerts
+    // }
   }
 })
 </script>
@@ -91,7 +125,7 @@ export default defineComponent({
 <style lang="postcss" scoped>
 @import "@/assets/styles/variables";
 
-.backdrop {
+.SPortalModals {
   position: fixed;
   top: 0;
   right: 0;
@@ -99,16 +133,15 @@ export default defineComponent({
   left: 0;
   z-index: var(--z-index-backdrop);
   background-color: rgba(0, 0, 0, .8);
-  opacity: 1;
 }
 
-.backdrop.fade-enter-active,
-.backdrop.fade-leave-active {
+.SPortalModals.fade-enter-active,
+.SPortalModals.fade-leave-active {
   transition: opacity .25s;
 }
 
-.backdrop.fade-enter,
-.backdrop.fade-leave-active {
+.SPortalModals.fade-enter,
+.SPortalModals.fade-leave-active {
   opacity: 0;
 }
 
