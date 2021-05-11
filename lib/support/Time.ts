427e48bf2575618ecf95bcc2696bac48699a7ffb
Cue <1493221+cuebit@users.noreply.github.com>
2021-05-11 02:32:08 +0100

    fix: avoid tuple and instead infer return type

--- lib/support/Time.ts
+++ lib/support/Time.ts
@@ -1,9 +1,9 @@
-export function sleep(ms: number = 500): Promise<void> {
-  return new Promise<undefined>((resolve) => {
-    setTimeout(resolve, ms)
-  })
+type Awaited<T> = T extends undefined ? T : T extends PromiseLike<infer U> ? U : T
+
+export function sleep(ms: number = 500): Promise<undefined> {
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-export function delay<T extends Promise<any>>(iterable: T[], ms?: number): Promise<T[]> {
-  return Promise.all([...iterable, sleep(ms)])
+export function delay<T extends any[]>(iterable: T, ms?: number): Promise<{ [P in keyof T]: Awaited<T[P]> }> {
+  return Promise.all([...iterable, sleep(ms)]) as any
 }
