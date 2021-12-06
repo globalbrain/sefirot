c485f7978a5c0a81f822f86ec34b807c052114d1
Kia King Ishii <kia.king.08@gmail.com>
2021-12-06 18:16:23 +0900

    wip: working on

---
+++ lib/components/SInputRadio.vue
@@ -0,0 +1,111 @@
+<template>
+  <SInputBase
+    class="SInputRadio"
+    :class="[size]"
+    :label="label"
+    :note="note"
+    :help="help"
+    :error-message="errorMessage"
+    :validation="validation"
+  >
+    <div class="container">
+      <div class="input" :class="{ on: modelValue }" role="button" @click="emitChange">
+        <div class="box">
+          <div class="check" />
+        </div>
+
+        <p class="text">{{ text }}</p>
+      </div>
+    </div>
+  </SInputBase>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue'
+import { Validation } from '../composables/Validation'
+import SInputBase from './SInputBase.vue'
+
+type Size = 'mini' | 'small' | 'medium'
+
+const props = defineProps({
+  size: { type: String as PropType<Size>, default: 'small' },
+  label: { type: String, default: null },
+  note: { type: String, default: null },
+  help: { type: String, default: null },
+  text: { type: String, required: true },
+  errorMessage: { type: Boolean, default: true },
+  modelValue: { type: Boolean, required: true },
+  validation: { type: Object as PropType<Validation>, default: null }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+function emitChange() {
+  emit('update:modelValue', !props.modelValue)
+}
+</script>
+
+<style lang="postcss" scoped>
+.container {
+  display: flex;
+}
+
+.input {
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 32px;
+
+  &:hover {
+    .box {
+      border-color: var(--c-text-1);
+    }
+  }
+}
+
+.input.on {
+  .box {
+    border-color: var(--c-text-1);
+    box-shadow: var(--shadow-depth-3);
+  }
+
+  .check {
+    opacity: 1;
+    transform: scale(.6);
+  }
+}
+
+.box {
+  position: relative;
+  border: 2px solid var(--c-text-3);
+  border-radius: 50%;
+  width: 18px;
+  height: 18px;
+  transition: border-color .25s, box-shadow .25s;
+}
+
+.check {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  width: 100%;
+  background-color: var(--c-text-1);
+  opacity: 0;
+  transform: scale(0);
+  transition: opacity .25s, transform .1s;
+}
+
+.text {
+  margin: 0;
+  padding-left: 12px;
+  line-height: 20px;
+  font-size: 14px;
+  font-weight: 500;
+}
+</style>
---
+++ lib/components/SInputRadios.vue
@@ -0,0 +1,82 @@
+<template>
+  <SInputBase
+    class="SInputRadios"
+    :class="[size]"
+    :label="label"
+    :note="note"
+    :help="help"
+    :error-message="errorMessage"
+    :validation="validation"
+  >
+    <div class="container">
+      <div class="row">
+        <div v-for="(option, index) in options" :key="index" class="col">
+          <SInputRadio
+            :text="option.label"
+            :model-value="isChecked(option.value)"
+            @update:model-value="handleChange(option.value)"
+          />
+        </div>
+      </div>
+    </div>
+  </SInputBase>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue'
+import { Validation } from '../composables/Validation'
+import SInputBase from './SInputBase.vue'
+import SInputRadio from './SInputRadio.vue'
+
+type Size = 'mini' | 'small' | 'medium'
+
+interface Option {
+  label: string
+  value: string | number | boolean
+}
+
+const props = defineProps({
+  size: { type: String as PropType<Size>, default: 'small' },
+  name: { type: String, default: null },
+  label: { type: String, default: null },
+  note: { type: String, default: null },
+  help: { type: String, default: null },
+  nullable: { type: Boolean, default: true },
+  options: { type: Array as PropType<Option[]>, required: true },
+  errorMessage: { type: Boolean, default: true },
+  modelValue: { type: [String, Number, Boolean], default: null },
+  validation: { type: Object as PropType<Validation>, default: null }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+function isChecked(value: string | number | boolean) {
+  return value === props.modelValue
+}
+
+function handleChange(value: string | number | boolean) {
+  if (value !== props.modelValue) {
+    emit('update:modelValue', value)
+
+    return
+  }
+
+  if (props.nullable) {
+    emit('update:modelValue', null)
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+.container {
+  display: flex;
+}
+
+.row {
+  margin: -2px -8px;
+}
+
+.col {
+  padding: 2px 8px;
+}
+</style>
