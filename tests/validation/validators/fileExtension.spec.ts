d5b06a66059f8c1a12ffdd8192403a18ff436f92
Ryo_gk <62658104+ryo-gk@users.noreply.github.com>
2022-01-26 19:08:41 +0900

    test: add validators tests

---
+++ tests/validation/validators/fileExtension.spec.ts
@@ -0,0 +1,23 @@
+import { fileExtension } from 'sefirot/validation/validators/fileExtension'
+
+describe('vaidation/validators/fileExtension', () => {
+  it('should validate if the file has valid extension', () => {
+    const file = new File(['foo'], 'foo.txt', { type: 'text/plain' })
+
+    expect(fileExtension(file, ['txt'])).toBe(true)
+    expect(fileExtension(file, ['txt', 'png'])).toBe(true)
+    expect(fileExtension(file, ['png'])).toBe(false)
+  })
+
+  it('should treat `jpg` extension with extra care', () => {
+    const jpg = new File(['foo'], 'foo.jpg', { type: 'text/plain' })
+    const jpeg = new File(['foo'], 'foo.jpeg', { type: 'text/plain' })
+    const JPG = new File(['foo'], 'foo.JPG', { type: 'text/plain' })
+    const JPEG = new File(['foo'], 'foo.JPEG', { type: 'text/plain' })
+
+    expect(fileExtension(jpg, ['jpg'])).toBe(true)
+    expect(fileExtension(jpeg, ['jpg'])).toBe(true)
+    expect(fileExtension(JPG, ['jpg'])).toBe(true)
+    expect(fileExtension(JPEG, ['jpg'])).toBe(true)
+  })
+})
---
+++ tests/validation/validators/requiredYmd.spec.ts
@@ -0,0 +1,14 @@
+import { requiredYmd } from 'sefirot/validation/validators/requiredYmd'
+
+describe('validation/validators/requiredYmd', () => {
+  it('should validate if the year, month, and date are present', () => {
+    expect(requiredYmd({ year: 2000, month: 12, date: 31 })).toBe(true)
+    expect(requiredYmd({ year: 2000, date: 2 })).toBe(false)
+  })
+
+  it('should validate only given types', () => {
+    const data = { year: 2000, month: 12 }
+
+    expect(requiredYmd(data, ['y', 'm'])).toBe(true)
+  })
+})
