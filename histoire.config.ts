b03d41a91c4bf15782a425febb8460ea1a2383ce
Ryo_gk <62658104+ryo-gk@users.noreply.github.com>
2022-07-29 16:33:37 +0900

    chore: set bg color presets

--- histoire.config.ts
+++ histoire.config.ts
@@ -1,9 +1,23 @@
 /// <reference types="histoire/vue" />
-import { defineConfig } from 'histoire'
+import { defaultColors, defineConfig } from 'histoire'
 
 export default defineConfig({
   setupFile: 'histoire.setup.ts',
   theme: {
     title: 'Sefirot',
-  }
+  },
+  backgroundPresets: [
+    {
+      label: 'Transparent',
+      color: 'transparent',
+    },
+    {
+      label: 'Light',
+      color: '#fff',
+    },
+    {
+      label: 'Dark',
+      color: '#171717'
+    }
+  ]
 })
