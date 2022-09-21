2e51c5a5683ce8709612e92f32ffb46fa70665cf
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2022-09-21 15:14:35 +0530

    refactor: move conditional stuff to s-icon itself

--- lib/components/SButton.vue
+++ lib/components/SButton.vue
@@ -62,8 +62,7 @@ function handleClick(): void {
     @click="handleClick"
   >
     <span class="content">
-      <span v-if="icon && 'body' in icon" class="icon"><SIcon :icon="icon" class="icon-svg" /></span>
-      <span v-else-if="icon" class="icon"><component :is="icon" class="icon-svg" /></span>
+      <span v-if="icon" class="icon"><SIcon :icon="icon" class="icon-svg" /></span>
       <span v-if="label" class="label">{{ label }}</span>
     </span>
 
--- lib/components/SIcon.vue
+++ lib/components/SIcon.vue
@@ -1,9 +1,11 @@
 <template>
-  <Icon :icon="icon" />
+  <Icon v-if="typeof icon['body'] === 'string'" :icon="(icon as IconifyIcon)" />
+  <component v-else :is="icon" />
 </template>
 
 <script setup lang="ts">
 import { Icon, IconifyIcon } from '@iconify/vue/dist/offline'
+import { DefineComponent } from 'vue'
 
-defineProps<{ icon: IconifyIcon }>()
+defineProps<{ icon: IconifyIcon | DefineComponent }>()
 </script>
