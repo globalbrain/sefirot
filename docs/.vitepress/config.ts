1b13b86025d18cee473b4130b3fee90956f9a02b
Kia Ishii <kia.king.08@gmail.com>
2022-09-22 13:42:57 +0900

    docs: add docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'Sefirot',
+  description: 'Vue Components for Global Brain Design System.',
+
+  themeConfig: {
+    editLink: {
+      pattern: 'https://github.com/globalbrain/sefirot/edit/master/docs/:path'
+    }
+  }
+})
