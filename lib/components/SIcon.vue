1b59d1549bcc9c74682e4bd2ecbd5e4ca2899f46
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2022-09-22 09:36:01 +0530

    feat(icon): add icon component and support using iconify in button (#140) (#141)
    ref #140
    
    Co-authored-by: Kia Ishii <kia.king.08@gmail.com>

---
+++ lib/components/SIcon.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Icon, IconifyIcon } from '@iconify/vue/dist/offline'
+import type { DefineComponent } from 'vue'
+
+defineProps<{
+  icon: IconifyIcon | DefineComponent
+}>()
+</script>
+
+<template>
+  <Icon v-if="typeof icon['body'] === 'string'" :icon="(icon as IconifyIcon)" />
+  <component v-else :is="icon" />
+</template>
