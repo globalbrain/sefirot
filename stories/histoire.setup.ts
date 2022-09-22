b2fbc2153fc0d1b5e0cafa71994ee4d05f84c182
Kia Ishii <kia.king.08@gmail.com>
2022-09-22 13:23:26 +0900

    chore: remove stuff that's not working

--- stories/histoire.setup.ts
+++ stories/histoire.setup.ts
@@ -3,13 +3,7 @@ import './styles.css'
 
 import { defineSetupVue3 } from '@histoire/plugin-vue'
 import { createPinia } from 'pinia'
-import { createStore } from 'vuex'
-import { Sefirot } from '../lib/store/Sefirot'
 
 export const setupVue3 = defineSetupVue3(({ app }) => {
   app.use(createPinia())
-
-  app.use(createStore({
-    plugins: [Sefirot()]
-  }))
 })
