89ca8c567e4358b373412ce8b1aaf7f4eff992d8
Kia King Ishii <kia.king.08@gmail.com>
2023-01-08 13:39:37 +0900

    test(button): increase coverage

--- tests/components/SButton.spec.ts
+++ tests/components/SButton.spec.ts
@@ -1,16 +1,54 @@
 import { mount } from '@vue/test-utils'
 import SButton from 'sefirot/components/SButton.vue'
+import { assertEmitted } from 'tests/Utils'
 
 describe('components/SButton', () => {
-  it('should emit on click', () => {
-    const wrapper = mount(SButton as any, {
-      propsData: {
+  test('tag is set to `button` by default', () => {
+    const wrapper = mount(SButton, {
+      props: {
         label: 'Button'
       }
     })
 
+    expect(wrapper.find('.SButton').element.tagName).toBe('BUTTON')
+  })
+
+  test('tag is set to `SLink` when `href` is set', () => {
+    const wrapper = mount(SButton, {
+      props: {
+        href: 'http://example.com'
+      }
+    })
+
+    expect(wrapper.find('.SButton').classes()).toContain('SLink')
+  })
+
+  test('tag is set to given `tag` if it exists', () => {
+    const wrapper = mount(SButton, {
+      props: {
+        tag: 'div'
+      }
+    })
+
+    expect(wrapper.find('.SButton').element.tagName).toBe('DIV')
+  })
+
+  test('tag is set to given `tag` if it exists even if `href` is set', () => {
+    const wrapper = mount(SButton, {
+      props: {
+        tag: 'div',
+        href: 'http://example.com'
+      }
+    })
+
+    expect(wrapper.find('.SButton').element.tagName).toBe('DIV')
+  })
+
+  test('it emits click event on click', () => {
+    const wrapper = mount(SButton)
+
     wrapper.find('.SButton').trigger('click')
 
-    expect(wrapper.emitted('click')).toBeTruthy()
+    assertEmitted(wrapper, 'click', 1, undefined)
   })
 })
