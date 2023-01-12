8fef54dbf1e8b6a6888cad5b214fad889e649ed9
Cue <hello@cuebit.dev>
2023-01-12 14:04:19 +0000

    fix: mock router (#184)

---
+++ stories/_support/Utils.ts
@@ -0,0 +1,12 @@
+import { Vue3StorySetupHandler } from '@histoire/plugin-vue'
+import { createMemoryHistory, createRouter, routerKey } from 'vue-router'
+
+export function setupRouter({ app }: Parameters<Vue3StorySetupHandler>[number]) {
+  app.provide(routerKey, createRouter({
+    history: createMemoryHistory(),
+    routes: [{
+      path: '/',
+      component: { render: () => null }
+    }]
+  }))
+}
