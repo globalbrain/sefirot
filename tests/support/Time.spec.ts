ef6c779fa0c06fd551dbd0618af0e4226d6af7c9
Cue <hello@cuebit.dev>
2023-01-12 14:17:06 +0000

    test: `sleep` coverage

--- tests/support/Time.spec.ts
+++ tests/support/Time.spec.ts
@@ -1,6 +1,32 @@
 import * as Time from 'sefirot/support/Time'
 
 describe('support/Time', () => {
+  describe('sleep', () => {
+    test('sleep for 500ms by default', async () => {
+      expect.assertions(1)
+
+      vi.useFakeTimers()
+
+      Time.sleep().then(() => {
+        expect(true).toBe(true)
+      })
+
+      vi.advanceTimersByTime(500)
+    })
+
+    test('sleep for a given time', async () => {
+      expect.assertions(1)
+
+      vi.useFakeTimers()
+
+      Time.sleep(1000).then(() => {
+        expect(true).toBe(true)
+      })
+
+      vi.advanceTimersByTime(1000)
+    })
+  })
+
   describe('delay', () => {
     test('delay callback execution for 500ms by default', async () => {
       expect.assertions(1)
