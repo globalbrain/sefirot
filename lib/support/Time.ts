09def4291f9b54449a35a659cb3201cb5528736f
Cue <hello@cuebit.dev>
2023-01-16 01:21:18 +0000

    test(support): `sleep` utility coverage (#185) (#187)
    close #185

--- lib/support/Time.ts
+++ lib/support/Time.ts
@@ -1,13 +1,9 @@
-type Awaited<T> = T extends undefined
-  ? T
-  : T extends PromiseLike<infer U> ? U : T
-
 export function sleep(ms = 500): Promise<undefined> {
   return new Promise<undefined>((resolve) => setTimeout(resolve, ms))
 }
 
-export function delay<T extends any[]>(
-  iterable: T,
+export function delay<T extends unknown[]>(
+  iterable: [...T],
   ms?: number
 ): Promise<{ [P in keyof T]: Awaited<T[P]> }> {
   return Promise.all([...iterable, sleep(ms)]) as any
