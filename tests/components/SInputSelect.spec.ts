5a5a57311a768ee577acd868da9f69738301ce02
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-01-16 06:54:14 +0530

    refactor(input-select): use index as option value (#183) (#188)
    close #183

--- tests/components/SInputSelect.spec.ts
+++ tests/components/SInputSelect.spec.ts
@@ -1,16 +1,16 @@
 import { mount } from '@vue/test-utils'
 import SInputSelect from 'sefirot/components/SInputSelect.vue'
-import { assertEmitted, assertNotEmitted, eventTargetValue } from 'tests/Utils'
+import { assertEmitted, assertNotEmitted } from 'tests/Utils'
 
-describe('components/SInputSelect', () => {
+describe('components/SInputSelect', async () => {
   test('it emits `update:model-value` and `change` events when item is selected', async () => {
     const wrapper = mount(SInputSelect, {
       props: {
-        options: []
+        options: [{ label: 'Item 001', value: 1 }]
       }
     })
 
-    ;(wrapper.vm as any).emitChange(eventTargetValue(JSON.stringify({ label: 'Item 001', value: 1 })))
+    await wrapper.find('select').setValue('0')
 
     assertEmitted(wrapper, 'update:model-value', 1, 1)
     assertEmitted(wrapper, 'change', 1, 1)
@@ -19,12 +19,12 @@ describe('components/SInputSelect', () => {
   test('it does not emit event when `disabled` is set', async () => {
     const wrapper = mount(SInputSelect, {
       props: {
-        options: [],
+        options: [{ label: 'Item 001', value: 1 }],
         disabled: true
       }
     })
 
-    ;(wrapper.vm as any).emitChange(eventTargetValue(JSON.stringify({ label: 'Item 001', value: 1 })))
+    await wrapper.find('select').setValue('0')
 
     assertNotEmitted(wrapper, 'update:model-value')
     assertNotEmitted(wrapper, 'change')
