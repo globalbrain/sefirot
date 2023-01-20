37c7625158444b4eb1805f6a62390a28af438b75
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-01-20 10:35:11 +0530

    add tests

---
+++ tests/validation/validators/maxFileSize.spec.ts
@@ -0,0 +1,21 @@
+import { maxFileSize } from 'sefirot/validation/validators/maxFileSize'
+
+describe('vaidation/validators/maxFileSize', () => {
+  it('should validate if the file is smaller than provided size', () => {
+    const file = new File([new ArrayBuffer(100_000)], 'foo.txt', { type: 'text/plain' })
+
+    expect(maxFileSize(file, '1 GB')).toBe(true)
+    expect(maxFileSize(file, '1 MB')).toBe(true)
+    expect(maxFileSize(file, '1 KB')).toBe(false)
+    expect(maxFileSize(file, '10 B')).toBe(false)
+  })
+
+  it('assumes size in bytes if no unit is provided', () => {
+    const file = new File([new ArrayBuffer(10_000)], 'foo.txt', { type: 'text/plain' })
+
+    expect(maxFileSize(file, '10 KB')).toBe(true)
+    expect(maxFileSize(file, '10000')).toBe(true)
+    expect(maxFileSize(file, '1 KB')).toBe(false)
+    expect(maxFileSize(file, '1000')).toBe(false)
+  })
+})
