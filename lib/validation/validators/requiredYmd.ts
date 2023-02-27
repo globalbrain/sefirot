f1aa5be40d97be38fc1ba2d4041c3238701bbf70
Kia King Ishii <kia.king.08@gmail.com>
2023-02-27 14:45:06 +0900

    refactor(validation): slightly wider validation for `requiredYmd`

--- lib/validation/validators/requiredYmd.ts
+++ lib/validation/validators/requiredYmd.ts
@@ -3,5 +3,5 @@ import { Ymd, YmdMap, YmdType } from './ymd'
 export type { Ymd, YmdType, YmdMap }
 
 export function requiredYmd(ymd: Ymd, required: YmdType[] = ['y', 'm', 'd']): boolean {
-  return required.every((r) => ymd[YmdMap[r]] !== null)
+  return required.every((r) => ymd[YmdMap[r]] != null)
 }
--- tests/validation/validators/requiredYmd.spec.ts
+++ tests/validation/validators/requiredYmd.spec.ts
@@ -1,12 +1,13 @@
 import { requiredYmd } from 'sefirot/validation/validators/requiredYmd'
 
 describe('validation/validators/requiredYmd', () => {
-  it('should validate if the year, month, and date are present', () => {
+  test('validates if the year, month, and date are present', () => {
+    expect(requiredYmd({ year: null, month: null, date: null })).toBe(false)
     expect(requiredYmd({ year: 2000, month: 12, date: 31 })).toBe(true)
     expect(requiredYmd({ year: 2000, month: null, date: 2 })).toBe(false)
   })
 
-  it('should validate only given types', () => {
+  test('validates only given types', () => {
     const data = { year: 2000, month: 12, date: null }
 
     expect(requiredYmd(data, ['y', 'm'])).toBe(true)
