7e3361cad4c033c72cb5c34342a6abb00a7498e3
Kia King Ishii <kia.king.08@gmail.com>
2023-02-28 10:54:49 +0900

    feat(dropdown): add `actions` type (#225) (#227)
    close #225

--- lib/components/SDropdownSection.vue
+++ lib/components/SDropdownSection.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { DropdownSection } from '../composables/Dropdown'
+import SDropdownSectionActions from './SDropdownSectionActions.vue'
 import SDropdownSectionComponent from './SDropdownSectionComponent.vue'
 import SDropdownSectionFilter from './SDropdownSectionFilter.vue'
 import SDropdownSectionMenu from './SDropdownSectionMenu.vue'
@@ -20,6 +21,10 @@ defineProps<{
     :selected="section.selected"
     :options="section.options"
     :on-click="section.onClick"
+  />
+  <SDropdownSectionActions
+    v-if="section.type === 'actions'"
+    :options="section.options"
   />
   <SDropdownSectionComponent
     v-else-if="section.type === 'component'"
