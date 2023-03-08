042abc9c615af491eb91fa6a6aa470112e7f1b08
Kia King Ishii <kia.king.08@gmail.com>
2023-03-09 01:39:15 +0900

    feat(day): add ymd and hms creation method

--- lib/support/Day.ts
+++ lib/support/Day.ts
@@ -4,12 +4,30 @@ import PluginRelativeTime from 'dayjs/plugin/relativeTime'
 import PluginTimezone from 'dayjs/plugin/timezone'
 import PluginUtc from 'dayjs/plugin/utc'
 
-export type Day = Dayjs
-export type Input = ConfigType
-
 dayjs.extend(PluginUtc)
 dayjs.extend(PluginTimezone)
 dayjs.extend(PluginRelativeTime)
+
+export type Day = Dayjs
+export type Input = ConfigType
+
+/**
+ * The year, month, and date object interface.
+ */
+export interface Ymd {
+  year: number | null
+  month: number | null
+  date: number | null
+}
+
+/**
+ * The hour, minute, and second object interface.
+ */
+export interface Hms {
+  hour: string | null
+  minute: string | null
+  second: string | null
+}
 
 export function day(input?: Input): Day {
   return dayjs(input)
@@ -21,4 +39,34 @@ export function utc(input?: Input): Day {
 
 export function tz(input?: Input, timezone?: string): Day {
   return dayjs.tz(input, timezone)
+}
+
+/**
+ * Creates a new `Ymd` object.
+ */
+export function createYmd(
+  year: number | null = null,
+  month: number | null = null,
+  date: number | null = null
+): Ymd {
+  return {
+    year,
+    month,
+    date
+  }
+}
+
+/**
+ * Creates a new `Hms` object.
+ */
+export function createHms(
+  hour: string | null = null,
+  minute: string | null = null,
+  second: string | null = null
+): Hms {
+  return {
+    hour,
+    minute,
+    second
+  }
 }
--- tests/support/Day.spec.ts
+++ tests/support/Day.spec.ts
@@ -2,13 +2,13 @@ import * as Day from 'sefirot/support/Day'
 
 describe('support/Day', () => {
   describe('day', () => {
-    test('it creates a new day instance', () => {
+    test('creates a new day instance', () => {
       expect(Day.day('1985-10-10').format('YYYY/MM/DD')).toBe('1985/10/10')
     })
   })
 
   describe('utc', () => {
-    test('it creates a new utc day instance', () => {
+    test('creates a new utc day instance', () => {
       const date = Day.utc('1985-10-10 00:00:00')
 
       expect(date.format('YYYY/MM/DD HH:mm:ss')).toBe('1985/10/10 00:00:00')
@@ -17,12 +17,56 @@ describe('support/Day', () => {
   })
 
   describe('tz', () => {
-    test('it creates a new timezone aware day instance', () => {
+    test('creates a new timezone aware day instance', () => {
       const def = Day.tz('1985-10-10 00:00:00')
       const tokyo = Day.tz('1985-10-10 00:00:00', 'Asia/Tokyo')
 
       expect(def.format('YYYY/MM/DD HH:mm:ss')).toBe('1985/10/10 00:00:00')
       expect(tokyo.format('YYYY/MM/DD HH:mm:ss')).toBe('1985/10/10 00:00:00')
+    })
+  })
+
+  describe('createYmd', () => {
+    test('creates a new ymd instance', () => {
+      const expected = {
+        year: 1985,
+        month: 10,
+        date: 10
+      }
+
+      expect(Day.createYmd(1985, 10, 10)).toEqual(expected)
+    })
+
+    test('all fields are `null` by default', () => {
+      const expected = {
+        year: null,
+        month: null,
+        date: null
+      }
+
+      expect(Day.createYmd()).toEqual(expected)
+    })
+  })
+
+  describe('createHms', () => {
+    test('creates a new hms instance', () => {
+      const expected = {
+        hour: '10',
+        minute: '20',
+        second: '30'
+      }
+
+      expect(Day.createHms('10', '20', '30')).toEqual(expected)
+    })
+
+    test('all fields are `null` by default', () => {
+      const expected = {
+        hour: null,
+        minute: null,
+        second: null
+      }
+
+      expect(Day.createHms()).toEqual(expected)
     })
   })
 })
