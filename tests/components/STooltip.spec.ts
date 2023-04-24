6408fcebde76a0c58be2db3b400900a859929a66
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-04-25 00:29:35 +0530

    test: add tests for hover/focus
    Co-authored-by: Cue <hello@cuebit.dev>

--- tests/components/STooltip.spec.ts
+++ tests/components/STooltip.spec.ts
@@ -30,4 +30,55 @@ describe('components/STooltip', () => {
 
     expect(wrapper.find('.STooltip .tip').text()).toBe('Example text.')
   })
+
+  test('it shows tooltip on hover', async () => {
+    vi.useFakeTimers()
+
+    const wrapper = mount(STooltip, {
+      attachTo: document.body,
+      props: {
+        text: 'Example text.'
+      }
+    })
+
+    expect(wrapper.find('.STooltip .tip').isVisible()).toBe(false)
+
+    await wrapper.find('.STooltip').trigger('mouseenter')
+    vi.advanceTimersByTime(1)
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('.STooltip .tip').isVisible()).toBe(true)
+
+    await wrapper.find('.STooltip').trigger('mouseleave')
+    vi.advanceTimersByTime(1)
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('.STooltip .tip').isVisible()).toBe(false)
+
+    vi.useRealTimers()
+  })
+
+  test('it shows tooltip on focus', async () => {
+    vi.useFakeTimers()
+
+    const wrapper = mount(STooltip, {
+      attachTo: document.body,
+      props: {
+        text: 'Example text.',
+        trigger: 'focus'
+      }
+    })
+
+    expect(wrapper.find('.STooltip .tip').isVisible()).toBe(false)
+
+    await wrapper.find('.STooltip').trigger('focusin')
+    vi.advanceTimersByTime(1)
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('.STooltip .tip').isVisible()).toBe(true)
+
+    await wrapper.find('.STooltip').trigger('focusout')
+    vi.advanceTimersByTime(1)
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('.STooltip .tip').isVisible()).toBe(false)
+
+    vi.useRealTimers()
+  })
 })
