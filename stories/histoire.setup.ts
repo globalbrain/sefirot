90e875a5f9e10b9523a178832791c77dbb80825b
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-04-26 06:10:51 +0530

    chore(story): fix router symbol not found warnings (#265) (#267)
    fix #265

--- stories/histoire.setup.ts
+++ stories/histoire.setup.ts
@@ -4,9 +4,12 @@ import './styles.css'
 import { defineSetupVue3 } from '@histoire/plugin-vue'
 import { createPinia } from 'pinia'
 import Board from './_support/Board.vue'
+import { setupRouter } from './_support/Utils'
 
 export const setupVue3 = defineSetupVue3(({ app }) => {
   app.use(createPinia())
 
   app.component('Board', Board)
+
+  setupRouter({ app })
 })
