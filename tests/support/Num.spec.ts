c489976f4ed4288d8fd45a3d389b28fe9e883355
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-05-26 13:27:26 +0530

    test: add tests for abbreviate

--- tests/support/Num.spec.ts
+++ tests/support/Num.spec.ts
@@ -8,4 +8,21 @@ describe('support/Num', () => {
       expect(Num.format(1000000)).toEqual('1,000,000')
     })
   })
+
+  describe('abbreviate', () => {
+    test('abbreviate given number', () => {
+      expect(Num.abbreviate(100)).toEqual('100')
+      expect(Num.abbreviate(1000)).toEqual('1K')
+      expect(Num.abbreviate(1000000)).toEqual('1M')
+      expect(Num.abbreviate(1000000000)).toEqual('1B')
+    })
+
+    test('abbreviate given number with precision', () => {
+      expect(Num.abbreviate(123, 2)).toEqual('123')
+      expect(Num.abbreviate(1234, 2)).toEqual('1.23K')
+      expect(Num.abbreviate(12345, 2)).toEqual('12.35K')
+      expect(Num.abbreviate(123456, 2)).toEqual('123.46K')
+      expect(Num.abbreviate(1234567, 2)).toEqual('1.23M')
+    })
+  })
 })
