b2a28be2c7ec1e292b22357cd0e1e9a424880eaf
Kia King Ishii <kia.king.08@gmail.com>
2023-05-29 14:25:42 +0900

    feat(table): add `menu` option

---
+++ lib/components/STableHeaderMenu.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { type TableMenu } from '../composables/Table'
+import { isArray } from '../support/Utils'
+import STableHeaderMenuItem from './STableHeaderMenuItem.vue'
+
+const props = defineProps<{
+  menu: TableMenu[] | TableMenu[][]
+}>()
+
+const normalizedMenu = computed(() => {
+  return isArray(props.menu[0])
+    ? props.menu as TableMenu[][]
+    : [props.menu] as TableMenu[][]
+})
+</script>
+
+<template>
+  <div class="STableHeaderMenu">
+    <div v-for="items, index in normalizedMenu" :key="index" class="group">
+      <div v-if="index > 0" class="divider" />
+      <div v-for="item in items" :key="item.label" class="item">
+        <STableHeaderMenuItem
+          :label="item.label"
+          :state="item.state"
+          :dropdown="item.dropdown"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.STableHeaderMenu {
+  display: flex;
+  align-items: center;
+  height: 48px;
+}
+
+.group {
+  display: flex;
+  align-items: center;
+  height: 48px;
+}
+
+.divider {
+  margin: 0 8px;
+  width: 1px;
+  height: 16px;
+  background-color: var(--c-divider-2);
+}
+</style>
