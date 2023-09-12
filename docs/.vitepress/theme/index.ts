17b381c2c3ec323221a726978f83701728941e4c
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-09-12 12:17:47 +0530

    chore: remove vue-virtual-scroller completely

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,6 @@
 import { Theme } from 'vitepress'
 import VitePressTheme from 'vitepress/theme'
 
-import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 import 'sefirot/styles/variables.css'
 import './styles.css'
 
