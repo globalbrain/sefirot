2929d4d28af6805f8e10a662982fd9f23431ecac
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-11-14 11:23:36 +0530

    chore: remove redundant version and date from release notes (#388)

---
+++ .release-it.js
@@ -0,0 +1,23 @@
+module.exports = {
+  git: {
+    commitMessage: 'release: v${version}',
+    tagName: 'v${version}'
+  },
+  github: {
+    release: true,
+    releaseName: 'v${version}',
+    releaseNotes(context) {
+      return context.changelog.split('\n').slice(1).join('\n').trim()
+    }
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      preset: 'angular',
+      infile: 'CHANGELOG.md',
+      ignoreRecommendedBump: true
+    }
+  },
+  hooks: {
+    'before:init': 'pnpm test:fail'
+  }
+}
