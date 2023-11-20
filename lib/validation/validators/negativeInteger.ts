bb60a16e079e4d305f3a25e7e8960ecdda91d28d
Nozomu Ikuta <16436160+NozomuIkuta@users.noreply.github.com>
2023-11-20 20:43:46 +0900

    fix: fix validator logic

--- lib/validation/validators/negativeInteger.ts
+++ lib/validation/validators/negativeInteger.ts
@@ -1,5 +1,3 @@
-const regExp = /^(?:\-(?:[1-9]+[0-9]*))$/
-
 export function negativeInteger(value: number): boolean {
-  return regExp.test(value.toString())
+  return Number.isInteger(value) && value < 0
 }
