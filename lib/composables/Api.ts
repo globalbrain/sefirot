ef0df469b268e1ceb56aebf14ac7ff110d7ada2e
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-12-07 13:09:34 +0530

    feat(api): add useQuery, useMutation, useGet composables

---
+++ lib/composables/Api.ts
@@ -0,0 +1,77 @@
+import { type ComputedRef, type ShallowRef, computed, ref } from 'vue'
+import { Http } from '../http/Http.js'
+
+export interface Query<Data = unknown> {
+  loading: ComputedRef<boolean>
+  data: ShallowRef<Data | undefined>
+  execute(): Promise<Data>
+}
+
+export interface UseQueryOptions {
+  // controls whether the query should execute immediately
+  // @default true
+  immediate?: boolean
+}
+
+export interface Mutation<Data = unknown, Args extends unknown[] = unknown[]> {
+  loading: ComputedRef<boolean>
+  data: ShallowRef<Data | undefined>
+  execute(...args: Args): Promise<Data>
+}
+
+export type Get<Data = unknown, Args extends unknown[] = unknown[]> = Mutation<Data, Args>
+
+export function useQuery<Data = unknown>(
+  req: (http: Http) => Promise<Data>,
+  options: UseQueryOptions = {}
+): Query<Data> {
+  const loading = ref(false)
+  const data = ref<Data | undefined>()
+
+  if (options.immediate !== false) {
+    execute()
+  }
+
+  async function execute(): Promise<Data> {
+    loading.value = true
+
+    const res: Data = await req(new Http())
+    data.value = res
+
+    loading.value = false
+    return res
+  }
+
+  return {
+    loading: computed(() => loading.value),
+    data,
+    execute
+  }
+}
+
+export function useMutation<Data = unknown, Args extends unknown[] = unknown[]>(
+  req: (http: Http, ...args: Args) => Promise<Data>
+): Mutation<Data, Args> {
+  const loading = ref(false)
+  const data = ref<Data | undefined>()
+
+  async function execute(...args: Args): Promise<Data> {
+    loading.value = true
+
+    const res: Data = await req(new Http(), ...args)
+    data.value = res
+
+    loading.value = false
+    return res
+  }
+
+  return {
+    loading: computed(() => loading.value),
+    data,
+    execute
+  }
+}
+
+export const useGet: <Data = unknown, Args extends unknown[] = unknown[]>(
+  req: (http: Http, ...args: Args) => Promise<Data>
+) => Get<Data, Args> = useMutation
