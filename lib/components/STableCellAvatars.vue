303f37750dafc2d86f03bd5c7b71e650165aac57
Kia King Ishii <kia.king.08@gmail.com>
2023-12-19 17:51:51 +0900

    feat(table): add option to control display count of avatars (#416) (#417)
    close #416 
    
    ---------
    
    Co-authored-by: Divyansh Singh <40380293+brc-dd@users.noreply.github.com>

--- lib/components/STableCellAvatars.vue
+++ lib/components/STableCellAvatars.vue
@@ -3,12 +3,17 @@ import { computed } from 'vue'
 import { type TableCellAvatarsOption } from '../composables/Table'
 import SAvatar from './SAvatar.vue'
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   value?: any
   record?: any
   avatars: TableCellAvatarsOption[] | ((value: any, record: any) => TableCellAvatarsOption[])
   color?: 'neutral' | 'soft' | 'mute'
-}>()
+  avatarCount?: number
+  nameCount?: number
+}>(), {
+  avatarCount: 2,
+  nameCount: 2
+})
 
 const _avatars = computed(() => {
   return typeof props.avatars === 'function'
@@ -16,39 +21,57 @@ const _avatars = computed(() => {
     : props.avatars
 })
 
-const displayAvatars = computed(() => {
-  return _avatars.value.slice(0, 3)
+const avatarDiff = computed(() => {
+  return _avatars.value.length - props.avatarCount
 })
 
-const names = computed(() => {
-  if (_avatars.value.length === 0) {
+const displayAvatars = computed(() => {
+  return _avatars.value.slice(0, props.avatarCount)
+})
+
+const nameDiff = computed(() => {
+  return _avatars.value.length - props.nameCount
+})
+
+const displayNames = computed(() => {
+  // If the count is 0, treat it as "disabling" the name display.
+  if (props.nameCount === 0) {
     return null
   }
 
-  if (_avatars.value.length === 1) {
-    return _avatars.value[0].name
+  const slicedAvatars = _avatars.value.slice(0, props.nameCount)
+
+  const names = slicedAvatars.map((avatar) => avatar.name).join(', ')
+
+  if (nameDiff.value > 0) {
+    return `${names}, ...`
   }
 
-  if (_avatars.value.length === 2) {
-    return `${_avatars.value[0].name}, ${_avatars.value[1].name}`
-  }
-
-  return `${_avatars.value[0].name}, ${_avatars.value[1].name} +${_avatars.value.length - 2}`
+  return names
 })
 </script>
 
 <template>
   <div class="STableCellAvatars" :class="[color]">
     <div class="container">
-      <div class="avatars">
-        <div v-for="(avatar, index) in displayAvatars" :key="index" class="avatar">
+      <div v-if="displayAvatars.length" class="avatars">
+        <div
+          v-for="(avatar, index) in displayAvatars"
+          :key="index"
+          class="avatar"
+        >
           <div class="avatar-box">
             <SAvatar size="mini" :avatar="avatar.image" :name="avatar.name" />
           </div>
         </div>
+        <div v-if="avatarDiff > 0" class="avatar">
+          <div class="avatar-box placeholder" :class="{ small: avatarDiff >= 10 }">
+            +{{ avatarDiff }}
+          </div>
+        </div>
       </div>
-      <div class="names">
-        {{ names }}
+      <div v-if="displayNames" class="names">
+        {{ displayNames }}
       </div>
     </div>
   </div>
@@ -72,28 +95,44 @@ const names = computed(() => {
 .avatar {
   position: relative;
   width: 20px;
+}
 
-  &:nth-child(1) { z-index: 30; }
-  &:nth-child(2) { z-index: 20; }
-  &:nth-child(3) { z-index: 10; }
+.avatar.placeholder {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .avatar-box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   border: 2px solid var(--c-bg-elv-3);
   border-radius: 50%;
   width: 28px;
+  height: 28px;
 
   .row:hover & {
     border: 2px solid var(--c-bg-elv-4);
+  }
+
+  &.placeholder {
+    font-size: 10px;
+    font-weight: 500;
+    color: var(--c-text-2);
+    background-color: var(--c-bg-mute-1);
+  }
+
+  &.placeholder.small {
+    font-size: 9px;
   }
 }
 
 .names {
   line-height: 28px;
   font-size: 12px;
-  font-weight: 500;
-
-  .STableCellAvatars.soft & { color: var(--c-text-2); }
-  .STableCellAvatars.mute & { color: var(--c-text-3); }
 }
+
+.STableCellAvatars.soft .names { color: var(--c-text-2); }
+.STableCellAvatars.mute .names { color: var(--c-text-3); }
 </style>
