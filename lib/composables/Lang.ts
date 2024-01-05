49edf204fce2d83d37c7d85aa6da7a25a8c2447f
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-01-05 10:48:13 +0530

    feat(lang): add useBrowserLang function

--- lib/composables/Lang.ts
+++ lib/composables/Lang.ts
@@ -32,3 +32,9 @@ export function useTrans<T>(messages: TransMessages<T>): Trans<T> {
     t
   }
 }
+
+export function useBrowserLang() {
+  const lang = navigator.language
+
+  return lang.split('-')[0] === 'ja' ? 'ja' : 'en'
+}
