0640afc076df494ff09473b97f945c2508dd67f6
Kia King Ishii <kia.king.08@gmail.com>
2024-01-05 11:59:24 +0900

    fix(d): make init work multiple times

--- lib/composables/D.ts
+++ lib/composables/D.ts
@@ -6,12 +6,12 @@ export interface D<T extends Record<string, any>> {
 }
 
 export function useD<T extends Record<string, any>>(data: T): D<T> {
-  const initialData = JSON.parse(JSON.stringify(data))
+  const initialData = JSON.stringify(data)
 
   const refData = ref(data) as Ref<T>
 
   function init(): void {
-    refData.value = initialData
+    refData.value = JSON.parse(initialData)
   }
 
   return {
--- tests/composables/D.spec.ts
+++ tests/composables/D.spec.ts
@@ -17,5 +17,17 @@ describe('composables/D', () => {
 
     expect(data.value.a).toBe(1)
     expect(data.value.b).toBe(2)
+
+    // Check 2nd time reset to ensure it can reset the state multiple times.
+    data.value.a = 3
+    data.value.b = 4
+
+    expect(data.value.a).toBe(3)
+    expect(data.value.b).toBe(4)
+
+    init()
+
+    expect(data.value.a).toBe(1)
+    expect(data.value.b).toBe(2)
   })
 })
