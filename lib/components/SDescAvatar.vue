ab662e62fff505d4feb0199e9465cd4e822a5f24
Kia King Ishii <kia.king.08@gmail.com>
2024-01-31 09:59:35 +0900

    feat(desc): add `<SDescAvatar>` (#455) (#458)
    close #455

---
+++ lib/components/SDescAvatar.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import SAvatar from './SAvatar.vue'
+import SDescEmpty from './SDescEmpty.vue'
+
+export interface Avatar {
+  avatar?: string | null
+  name?: string | null
+}
+
+defineProps<{
+  avatar?: Avatar | null
+}>()
+</script>
+
+<template>
+  <div v-if="avatar" class="SDescAvatar">
+    <div class="value">
+      <SAvatar
+        v-if="avatar.avatar"
+        size="nano"
+        :avatar="avatar.avatar"
+        :name="avatar.name"
+      />
+      <span v-if="avatar.name" class="name">
+        {{ avatar.name }}
+      </span>
+    </div>
+  </div>
+  <SDescEmpty v-else />
+</template>
+
+<style scoped lang="postcss">
+.value {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  height: 24px;
+}
+</style>
