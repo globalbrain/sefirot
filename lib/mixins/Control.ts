e63d74829e7ec47b9249dedb9e7e592f876582ce
Kia King Ishii <kia.king.08@gmail.com>
2024-02-05 10:56:34 +0900

    add mixins

--- lib/mixins/Control.ts
+++ lib/mixins/Control.ts
@@ -1,5 +1,9 @@
 import { type App } from 'vue'
 import SControl from '../components/SControl.vue'
+import SControlActionBar from '../components/SControlActionBar.vue'
+import SControlActionBarButton from '../components/SControlActionBarButton.vue'
+import SControlActionBarClose from '../components/SControlActionBarClose.vue'
+import SControlActionBarCollapse from '../components/SControlActionBarCollapse.vue'
 import SControlActionMenu from '../components/SControlActionMenu.vue'
 import SControlButton from '../components/SControlButton.vue'
 import SControlCenter from '../components/SControlCenter.vue'
@@ -11,6 +15,10 @@ import SControlText from '../components/SControlText.vue'
 
 export function mixin(app: App): void {
   app.component('SControl', SControl)
+  app.component('SControlActionBar', SControlActionBar)
+  app.component('SControlActionBarButton', SControlActionBarButton)
+  app.component('SControlActionBarClose', SControlActionBarClose)
+  app.component('SControlActionBarCollapse', SControlActionBarCollapse)
   app.component('SControlActionMenu', SControlActionMenu)
   app.component('SControlButton', SControlButton)
   app.component('SControlCenter', SControlCenter)
@@ -24,6 +32,9 @@ export function mixin(app: App): void {
 declare module 'vue' {
   export interface GlobalComponents {
     SControl: typeof SControl
+    SControlActionBar: typeof SControlActionBar
+    SControlActionBarButton: typeof SControlActionBarButton
+    SControlActionBarClose: typeof SControlActionBarClose
     SControlActionMenu: typeof SControlActionMenu
     SControlButton: typeof SControlButton
     SControlCenter: typeof SControlCenter
