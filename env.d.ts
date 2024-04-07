ce62237c7dd119cf4c84cd25c20d7af7d58b45d9
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-04-08 00:34:36 +0530

    shims for vue are not needed

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,5 @@
 /// <reference types="vite/client" />
 /// <reference types="@histoire/plugin-vue/components.d.ts" />
 
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-
 declare const __STORY_HOST__: string
 declare const __DOCS_HOST__: string
