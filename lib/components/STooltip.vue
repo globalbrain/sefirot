511b320e774339d7e8861da32d135d1ee5656145
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-04-08 13:53:01 +0530

    update condition

--- lib/components/STooltip.vue
+++ lib/components/STooltip.vue
@@ -58,7 +58,7 @@ const cleanup2 = onClickOutside(root, hide, { ignore: [content] })
 onBeforeUnmount(() => {
   cleanup1()
   cleanup2()
-  timeoutId.value && window.clearTimeout(timeoutId.value)
+  timeoutId.value != null && window.clearTimeout(timeoutId.value)
 })
 
 const isRootHovered = useElementHover(root, { delayLeave: 100 })
