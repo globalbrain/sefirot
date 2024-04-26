ac83a51d8884228e2840bc82f5e95ad1558270a6
Nozomu Ikuta <16436160+NozomuIkuta@users.noreply.github.com>
2024-04-26 14:12:18 +0900

    feat(control): add `SControlButtonGroup` component

---
+++ lib/components/SControlButtonGroup.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="SControlButtonGroup">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.SControlButtonGroup :deep(.SControlButton) {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+
+  .SButton {
+    border-left-width: 0;
+    border-radius: 0;
+  }
+
+  &:first-child .SButton {
+    border-left-width: 1px;
+    border-radius: 6px 0 0 6px;
+  }
+
+  &:last-child .SButton {
+    border-radius: 0 6px 6px 0;
+  }
+}
+</style>
