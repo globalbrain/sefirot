d81fdf64d7fecf1d545fb117ddbb3ba00a125063
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-07-21 14:35:44 +0530

    remove redundant code

--- config/vite.js
+++ config/vite.js
@@ -13,66 +13,10 @@
 
 import icons from 'unplugin-icons/vite'
 import { mergeConfig } from 'vite'
-import { compileTemplate } from 'vue/compiler-sfc'
-
-const randIdFn = 'const __randId = () => Math.random().toString(36).substr(2, 10);'
-
-/**
- * @param {string} svg
- * @returns {{ hasID: boolean, svg: string, injectScripts: string }}
- */
-function handleSVGId(svg) {
-  const hasID = /="url\(#/.test(svg)
-  /** @type {Record<string, string>} */
-  const idMap = {}
-  let injectScripts = ''
-
-  if (hasID) {
-    svg = svg
-      .replace(/\b([\w-]+?)="url\(#(.+?)\)"/g, (_, s, id) => {
-        idMap[id] = `'${id}':'uicons-'+__randId()`
-        return `:${s}="'url(#'+idMap['${id}']+')'"`
-      })
-      .replace(/\bid="(.+?)"/g, (full, id) => {
-        if (idMap[id]) { return `:id="idMap['${id}']"` }
-        return full
-      })
-
-    injectScripts = `${randIdFn}const idMap = {${Object.values(idMap).join(',')}};`
-  }
-
-  return { hasID, svg, injectScripts }
-}
-
-/**
- * @param {string} svg
- * @param {string} collection
- * @param {string} icon
- * @returns {string}
- */
-function compiler(svg, collection, icon) {
-  const { injectScripts, svg: handled } = handleSVGId(svg)
-
-  let { code } = compileTemplate({
-    source: handled,
-    id: `${collection}:${icon}`,
-    filename: `${collection}-${icon}.vue`
-  })
-
-  code = `import { markRaw } from 'vue'\n${code}`
-  code = code.replace(/^export /gm, '')
-  code += `\n\nexport default markRaw({ name: '${collection}-${icon}', render${
-    injectScripts ? `, data() {${injectScripts};return { idMap }}` : ''
-  } })`
-  code += '\n/* vite-plugin-components disabled */'
-
-  return code
-}
 
 /** @type {import('vite').UserConfig} */
 export const baseConfig = {
-  // TODO:: remove custom compiler when unplugin-icons is updated
-  plugins: [icons({ scale: 1, compiler: { compiler } })],
+  plugins: [icons({ scale: 1 })],
 
   resolve: {
     alias: { 'sefirot/': new URL('../lib/', import.meta.url).pathname },
