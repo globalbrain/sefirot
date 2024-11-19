5b5422a52f68c532bb5b3c3d5544af76c89d615c
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-11-19 10:34:29 +0530

    chore: adjust eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,9 @@ module.exports = {
 
   rules: {
     'vue/no-template-shadow': 'off',
-    'no-console': 'warn'
+    'no-console': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'warn',
+    'unused-imports/no-unused-vars': 'warn'
   }
 }
