cde4e2d5dc64a8e12ef73143d843719bdf383ed1
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2025-06-05 14:03:35 +0530

    add html prop

--- lib/components/SMarkdown.vue
+++ lib/components/SMarkdown.vue
@@ -2,29 +2,23 @@
 import { computed } from 'vue'
 import { useMarkdown } from '../composables/Markdown'
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   tag?: string
   content: string
+  html?: boolean
   inline?: boolean
-}>()
+}>(), {
+  tag: 'div',
+  html: true
+})
 
-const markdown = useMarkdown({ inline: props.inline })
+const markdown = useMarkdown({ html: props.html, inline: props.inline })
 const rendered = computed(() => markdown(props.content))
 </script>
 
 <template>
-  <component
-    v-if="$slots.default"
-    :is="tag ?? 'div'"
-    class="SMarkdown-container"
-  >
+  <component v-if="$slots.default" :is="tag" class="SMarkdown-container">
     <slot v-bind="{ rendered }" />
   </component>
-
-  <component
-    v-else
-    :is="tag ?? 'div'"
-    class="SMarkdown-container"
-    v-html="rendered"
-  />
+  <component v-else :is="tag" class="SMarkdown-container" v-html="rendered" />
 </template>
