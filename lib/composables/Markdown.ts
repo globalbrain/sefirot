e15c036e229d86a024b7f1c6881a02a82d227c4a
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2025-07-18 15:54:01 +0530

    fix(markdown, content): set correct counter in ordered lists, render ordered list marker correctly, and allow passing advanced options (#633)

--- lib/composables/Markdown.ts
+++ lib/composables/Markdown.ts
@@ -1,7 +1,70 @@
 import DOMPurify, { type Config } from 'dompurify'
-import MarkdownIt, { type Options as MarkdownItOptions } from 'markdown-it'
+import MarkdownIt from 'markdown-it'
 
 export type UseMarkdown = (source: string, inline?: boolean) => string
+
+// vendored for vue compatibility
+export interface MarkdownItOptions {
+  /**
+   * Set `true` to enable HTML tags in source. Be careful!
+   * That's not safe! You may need external sanitizer to protect output from XSS.
+   * It's better to extend features via plugins, instead of enabling HTML.
+   * @default false
+   */
+  html?: boolean | undefined
+
+  /**
+   * Set `true` to add '/' when closing single tags
+   * (`<br />`). This is needed only for full CommonMark compatibility. In real
+   * world you will need HTML output.
+   * @default false
+   */
+  xhtmlOut?: boolean | undefined
+
+  /**
+   * Set `true` to convert `\n` in paragraphs into `<br>`.
+   * @default false
+   */
+  breaks?: boolean | undefined
+
+  /**
+   * CSS language class prefix for fenced blocks.
+   * Can be useful for external highlighters.
+   * @default 'language-'
+   */
+  langPrefix?: string | undefined
+
+  /**
+   * Set `true` to autoconvert URL-like text to links.
+   * @default false
+   */
+  linkify?: boolean | undefined
+
+  /**
+   * Set `true` to enable [some language-neutral replacement](https://github.com/markdown-it/markdown-it/blob/master/lib/rules_core/replacements.js) +
+   * quotes beautification (smartquotes).
+   * @default false
+   */
+  typographer?: boolean | undefined
+
+  /**
+   * Double + single quotes replacement
+   * pairs, when typographer enabled and smartquotes on. For example, you can
+   * use `'«»„“'` for Russian, `'„“‚‘'` for German, and
+   * `['«\xA0', '\xA0»', '‹\xA0', '\xA0›']` for French (including nbsp).
+   * @default '“”‘’'
+   */
+  quotes?: string | string[]
+
+  /**
+   * Highlighter function for fenced code blocks.
+   * Highlighter `function (str, lang, attrs)` should return escaped HTML. It can
+   * also return empty string if the source was not changed and should be escaped
+   * externally. If result starts with <pre... internal wrapper is skipped.
+   * @default null
+   */
+  highlight?: ((str: string, lang: string, attrs: string) => string) | null | undefined
+}
 
 export interface UseMarkdownOptions extends MarkdownItOptions {
   config?: (md: MarkdownIt) => void
@@ -40,6 +103,16 @@ export function useMarkdown({
   //
 
   const md = new MarkdownIt({ html: true, linkify: true, ...options })
+
+  md.renderer.rules.ordered_list_open = (tokens, idx, options, env, self) => {
+    const token = tokens[idx]
+    const start = Number(token.attrGet('start')) - 1
+    if (start >= 0) {
+      token.attrSet('style', `counter-reset: s-medium-counter ${start}`)
+    }
+    return self.renderToken(tokens, idx, options)
+  }
+
   config?.(md)
 
   return (source, inline = _inline) => {
