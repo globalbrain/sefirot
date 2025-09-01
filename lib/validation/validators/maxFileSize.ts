345e8b9fd5905c6a75dae94ae1838fa426fd236b
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2025-09-02 00:53:18 +0530

    update hyphen and maxfilesize spec

--- lib/validation/validators/maxFileSize.ts
+++ lib/validation/validators/maxFileSize.ts
@@ -1,13 +1,7 @@
 export function maxFileSize(value: unknown, size: string): boolean {
   if (!(value instanceof File)) { return false }
 
-  const factor = /gb/i.test(size)
-    ? 1e9
-    : /mb/i.test(size)
-      ? 1e6
-      : /kb/i.test(size)
-        ? 1e3
-        : 1
+  const factor = /gb/i.test(size) ? 1e9 : /mb/i.test(size) ? 1e6 : /kb/i.test(size) ? 1e3 : 1
 
   return value.size <= factor * +size.replace(/[^\d\.]/g, '')
 }
--- tests/validation/validators/hyphen.spec.ts
+++ tests/validation/validators/hyphen.spec.ts
@@ -1,16 +1,33 @@
 import { hyphen } from 'sefirot/validation/validators'
 
 describe('validation/validators/hyphen', () => {
-  it('should validates if the value is true', () => {
+  it('returns true only for the string "-"', () => {
     expect(hyphen('-')).toBe(true)
+    expect(hyphen(String.fromCharCode(0x2D))).toBe(true)
+  })
 
+  it('returns false for similar strings', () => {
+    expect(hyphen(' - ')).toBe(false) // with spaces
+    expect(hyphen('_')).toBe(false) // underscore
+    expect(hyphen('－')).toBe(false) // fullwidth hyphen-minus
+    expect(hyphen('﹣')).toBe(false) // small hyphen-minus
+    expect(hyphen('‐')).toBe(false) // hyphen
+    expect(hyphen('‑')).toBe(false) // non-breaking hyphen
+    expect(hyphen('–')).toBe(false) // en dash
+    expect(hyphen('—')).toBe(false) // em dash
+    expect(hyphen('―')).toBe(false) // horizontal bar
+    expect(hyphen('−')).toBe(false) // minus sign
+    expect(hyphen('₋')).toBe(false) // subscript minus
+    expect(hyphen('⁻')).toBe(false) // superscript minus
+    expect(hyphen('➖')).toBe(false) // heavy minus sign
+  })
+
+  it('returns false for non-string types', () => {
     expect(hyphen(undefined)).toBe(false)
     expect(hyphen(null)).toBe(false)
+    expect(hyphen(45)).toBe(false)
     expect(hyphen(true)).toBe(false)
-    expect(hyphen(false)).toBe(false)
-    expect(hyphen(1)).toBe(false)
-    expect(hyphen('abc')).toBe(false)
-    expect(hyphen({})).toBe(false)
     expect(hyphen([])).toBe(false)
+    expect(hyphen({})).toBe(false)
   })
 })
