b1d2fa7f89055791048707b3bcfe956dc00aa37e
Kia King Ishii <kia.king.08@gmail.com>
2026-02-21 00:00:07 +0900

    feat(lens): add `settings` to lens query

--- lib/blocks/lens/LensQuery.ts
+++ lib/blocks/lens/LensQuery.ts
@@ -1,10 +1,18 @@
+import { type Lang } from '../../composables/Lang'
+
 export interface LensQuery {
   entity: string
   select: string[]
   filters: any[]
   sort: LensQuerySort[]
+  settings?: LensQuerySettings
   page: number
   perPage: number
 }
 
 export type LensQuerySort = [key: string, direction: 'asc' | 'desc']
+
+export interface LensQuerySettings {
+  lang?: Lang
+  tz?: string
+}
